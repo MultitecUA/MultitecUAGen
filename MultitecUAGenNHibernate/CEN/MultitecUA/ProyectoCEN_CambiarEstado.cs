c1d1119870b695bc57bd843334854cc62c970f63
Nedyar <Nedyar@hotmail.es>
2018-11-09 20:49:52 +0100

    Todo listo para la primera entrega

---
+++ MultitecUAGenNHibernate/CEN/MultitecUA/ProyectoCEN_CambiarEstado.cs
@@ -0,0 +1,35 @@
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Exceptions;
+using MultitecUAGenNHibernate.Exceptions;
+using MultitecUAGenNHibernate.EN.MultitecUA;
+using MultitecUAGenNHibernate.CAD.MultitecUA;
+
+
+/*PROTECTED REGION ID(usingMultitecUAGenNHibernate.CEN.MultitecUA_Proyecto_cambiarEstado) ENABLED START*/
+//  references to other libraries
+/*PROTECTED REGION END*/
+
+namespace MultitecUAGenNHibernate.CEN.MultitecUA
+{
+public partial class ProyectoCEN
+{
+public void CambiarEstado (int p_Proyecto_OID, MultitecUAGenNHibernate.Enumerated.MultitecUA.EstadoProyectoEnum p_estado)
+{
+            /*PROTECTED REGION ID(MultitecUAGenNHibernate.CEN.MultitecUA_Proyecto_cambiarEstado) ENABLED START*/
+
+            ProyectoCAD proyectoCAD = new ProyectoCAD();
+            ProyectoEN proyectoEN = proyectoCAD.ReadOIDDefault(p_Proyecto_OID);
+            proyectoEN.Estado = p_estado;
+
+            proyectoCAD.Modify(proyectoEN);
+
+            /*PROTECTED REGION END*/
+        }
+}
+}
---
+++ MultitecUAGenNHibernate/CEN/MultitecUA/SolicitudCEN_Rechazar.cs
@@ -0,0 +1,35 @@
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Exceptions;
+using MultitecUAGenNHibernate.Exceptions;
+using MultitecUAGenNHibernate.EN.MultitecUA;
+using MultitecUAGenNHibernate.CAD.MultitecUA;
+
+
+/*PROTECTED REGION ID(usingMultitecUAGenNHibernate.CEN.MultitecUA_Solicitud_rechazar) ENABLED START*/
+//  references to other libraries
+/*PROTECTED REGION END*/
+
+namespace MultitecUAGenNHibernate.CEN.MultitecUA
+{
+public partial class SolicitudCEN
+{
+public void Rechazar (int p_oid)
+{
+            /*PROTECTED REGION ID(MultitecUAGenNHibernate.CEN.MultitecUA_Solicitud_rechazar) ENABLED START*/
+
+            SolicitudCAD solicitudCAD = new SolicitudCAD();
+            SolicitudEN solicitudEN = solicitudCAD.ReadOIDDefault(p_oid);
+            solicitudEN.Estado = Enumerated.MultitecUA.EstadoSolicitudEnum.Rechazada;
+
+            solicitudCAD.ModifyDefault(solicitudEN);
+
+            /*PROTECTED REGION END*/
+        }
+}
+}
