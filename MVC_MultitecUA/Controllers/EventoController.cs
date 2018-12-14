9cc0a14088b45d7dbe1bd804052967865ef13bcc
Nedyar <Nedyar@hotmail.es>
2018-12-14 22:26:13 +0100

    Preparando todo para entregar

--- MVC_MultitecUA/Controllers/EventoController.cs
+++ MVC_MultitecUA/Controllers/EventoController.cs
@@ -1,9 +1,7 @@
-﻿using MultitecUAGenNHibernate.CAD.MultitecUA;
-using MultitecUAGenNHibernate.CEN.MultitecUA;
+﻿using MultitecUAGenNHibernate.CEN.MultitecUA;
 using MultitecUAGenNHibernate.CP.MultitecUA;
 using MultitecUAGenNHibernate.EN.MultitecUA;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,53 +14,34 @@ namespace MVC_MultitecUA.Controllers
         // GET: Evento
         public ActionResult Index()
         {
-            SessionInitialize();
-            EventoCAD cadEven = new EventoCAD(session);
-            EventoCEN eventoCEN = new EventoCEN(cadEven);
-            IList<EventoEN> listaEventoEn = eventoCEN.ReadAll(0, -1).ToList();
-            //IEnumerable<Servicio> listaServicios = new AssemblerServicio().ConvertListENToModel(listaServiciosEn).ToList();
-            SessionClose();
-            return View(listaEventoEn);
+            EventoCEN eventoCEN = new EventoCEN();
+            IList<EventoEN> listaEventos = eventoCEN.ReadAll(0, -1).ToList();
+            return View(listaEventos);
         }
 
         // GET: Evento/Details/5
         public ActionResult Details(int id)
         {
-            SessionInitialize();
-            ArrayList listaCates = new ArrayList();
-            CategoriaProyectoCEN categoriasP = new CategoriaProyectoCEN();
-            List<CategoriaProyectoEN> cat = categoriasP.ReadAll(0, -1).ToList();
-            foreach (CategoriaProyectoEN a in cat)
-            {
-                listaCates.Add(a.Id);
-            }
-            ViewData["listaCategoriasP"] = listaCates;
-            EventoCAD servicioCAD = new EventoCAD(session);
-            EventoCEN eventoCEN = new EventoCEN(servicioCAD);
-            EventoEN eventoEN = new EventoCEN().ReadOID(id);
-            ViewData["NombreEvento"] = eventoEN.Nombre;
-            ViewData["IdEvento"] = id;
-            SessionClose();
+            EventoCEN eventoCEN = new EventoCEN();
+            EventoEN eventoEN = eventoCEN.ReadOID(id);
             return View(eventoEN);
         }
 
         // GET: Evento/Create
         public ActionResult Create()
         {
-            EventoEN evento = new EventoEN();
-            return View(evento);
+            EventoEN eventoEN = new EventoEN();
+            return View(eventoEN);
         }
 
         // POST: Evento/Create
         [HttpPost]
-        public ActionResult Create(EventoEN evento)
+        public ActionResult Create(EventoEN eventoEN)
         {
             try
             {
-                // TODO: Add insert logic here
                 EventoCEN eventoCEN = new EventoCEN();
-                int OIDEvento = eventoCEN.New_(evento.Nombre, evento.Descripcion, evento.FechaInicio, evento.FechaFin, evento.FechaInicioInscripcion, evento.FechaTopeInscripcion, null);
-                eventoCEN.PublicaEvento(OIDEvento);
+                eventoCEN.New_(eventoEN.Nombre, eventoEN.Descripcion, eventoEN.FechaInicio, eventoEN.FechaFin, eventoEN.FechaInicioInscripcion, eventoEN.FechaTopeInscripcion, eventoEN.Fotos);
                 return RedirectToAction("Index");
             }
             catch
@@ -74,24 +53,19 @@ namespace MVC_MultitecUA.Controllers
         // GET: Evento/Edit/5
         public ActionResult Edit(int id)
         {
-            SessionInitialize();
-            EventoEN eventoEN = new EventoCAD(session).ReadOID(id);
-            SessionClose();
+            EventoCEN eventoCEN = new EventoCEN();
+            EventoEN eventoEN = eventoCEN.ReadOID(id);
             return View(eventoEN);
         }
 
         // POST: Evento/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, EventoEN evento)
+        public ActionResult Edit(int id, EventoEN eventoEN)
         {
             try
             {
-                // TODO: Add update logic here
-                SessionInitialize();
-                EventoCP eve = new EventoCP(session);
-                eve.Modify(id, evento.Nombre, evento.Descripcion, evento.FechaInicio, evento.FechaFin, evento.FechaInicioInscripcion, evento.FechaTopeInscripcion, null);
-                ViewData["NombreEvento"] = evento.Nombre;
-                SessionClose();
+                EventoCP eventoCP = new EventoCP();
+                eventoCP.Modify(id, eventoEN.Nombre, eventoEN.Descripcion, eventoEN.FechaInicio, eventoEN.FechaFin, eventoEN.FechaInicioInscripcion, eventoEN.FechaTopeInscripcion, eventoEN.Fotos);
                 return RedirectToAction("Index");
             }
             catch
@@ -105,22 +79,17 @@ namespace MVC_MultitecUA.Controllers
         {
             EventoCEN eventoCEN = new EventoCEN();
             EventoEN eventoEN = eventoCEN.ReadOID(id);
-            ViewData["NombreEvento"] = eventoEN.Nombre;
-            SessionClose();
             return View(eventoEN);
         }
 
         // POST: Evento/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, EventoEN evento)
+        public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
-                // TODO: Add delete logic here
-                SessionInitialize();
-                EventoCP eventoCP = new EventoCP(session);
+                EventoCP eventoCP = new EventoCP();
                 eventoCP.Destroy(id);
-                SessionClose();
                 return RedirectToAction("Index");
             }
             catch
@@ -128,109 +97,5 @@ namespace MVC_MultitecUA.Controllers
                 return View();
             }
         }
-
-        public ActionResult PorFiltro()
-        {
-            SessionInitialize();
-            EventoEN evento = new EventoEN();
-            //CategoriaProyectoCEN categoriasP = new CategoriaProyectoCEN();
-            //IList<CategoriaProyectoEN> categoriasProyecto = categoriasP.ReadAll(0, -1).ToList();
-            //List<string> listaCateP = new List<string>();
-            //foreach (CategoriaProyectoEN a in categoriasProyecto)
-            //{
-            //listaCateP.Add(a.Nombre);
-            //}
-            //ViewData["listaCateP"] = listaCateP;
-            SessionClose();
-            return View(evento);
-        }
-
-        public ActionResult Filtrar(FormCollection f)
-        {
-            SessionInitialize();
-            //if (f["categoria"] != null)
-            //{
-            /*string[] ps = f["categoria"].Split(',');
-            List<string> a = new List<string>();
-            for (byte i = 0; i < ps.Length; i++)
-            {
-                a.Add(ps[i]);
-            }*/
-
-            //}
-            EventoCAD eventoCAD = new EventoCAD(session);
-            EventoCEN evento = new EventoCEN(eventoCAD);
-            int num = int.Parse(f["Categoria"]);
-            IList<EventoEN> listaEventos;
-
-            if (f["FechaAnterior"] == "" && f["FechaFinal"] == "")
-            {
-                listaEventos = evento.DameEventosFiltrados(num, null, null);
-            }
-            else if (f["FechaAnterior"] == "")
-            {
-                DateTime ff = DateTime.Parse(f["FechaFinal"]);
-                listaEventos = evento.DameEventosFiltrados(num, null, ff);
-            }
-            else if (f["FechaFinal"] == "")
-            {
-                DateTime fa = DateTime.Parse(f["FechaAnterior"]);
-                listaEventos = evento.DameEventosFiltrados(num, fa, null);
-            }
-            else
-            {
-                DateTime ff = DateTime.Parse(f["FechaFinal"]);
-                DateTime fa = DateTime.Parse(f["FechaAnterior"]);
-                listaEventos = evento.DameEventosFiltrados(num, fa, null);
-            }
-
-            SessionClose();
-            return View(listaEventos);
-        }
-
-
-        [HttpPost]
-        public ActionResult AgregarCat(int id, FormCollection f)
-        {
-            SessionInitialize();
-            if (f["Categoria"] != "")
-            {
-                int num = id;//int.Parse(f["IdEvento"]);
-                List<int> categorias = new List<int>();
-                categorias.Add(int.Parse(f["Categoria"]));
-                EventoCAD eventoCAD = new EventoCAD(session);
-                EventoCEN evento = new EventoCEN(eventoCAD);
-                evento.AgregaCategorias(num, categorias);
-                return RedirectToAction("Details", new { id });
-            }
-            SessionClose();
-            return RedirectToAction("Details", new { id });
-
-
-        }
-
-
-        [HttpPost]
-        public ActionResult EliminarCat(int id, FormCollection f)
-        {
-            SessionInitialize();
-            if (f["Categoria"] != "")
-            {
-                int num = id;//int.Parse(f["IdEvento"]);
-                List<int> categorias = new List<int>();
-                categorias.Add(int.Parse(f["Categoria"]));
-                EventoCAD eventoCAD = new EventoCAD(session);
-                EventoCEN evento = new EventoCEN(eventoCAD);
-                evento.EliminaCategorias(num, categorias);
-                return RedirectToAction("Details", new { id });
-            }
-            SessionClose();
-            return RedirectToAction("Details", new { id });
-
-
-        }
-
-
-
     }
 }
