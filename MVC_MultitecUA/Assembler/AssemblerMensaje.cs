fe965986a31e10a04d6096a491d5efe12177e96a
Nedyar <Nedyar@hotmail.es>
2018-12-31 04:38:59 +0100

    Cambio masivo de Vistas

--- MVC_MultitecUA/Assembler/AssemblerMensaje.cs
+++ MVC_MultitecUA/Assembler/AssemblerMensaje.cs
@@ -22,15 +22,16 @@ namespace MVC_MultitecUA.Models
              serv.Estado = mensaje.Estado;*/
 
             mensj.Id = mensaje.Id;
+            mensj.Titulo = mensaje.Titulo;
+            mensj.Cuerpo = mensaje.Cuerpo;
+            mensj.EstadoLectura = mensaje.EstadoLecutra.ToString();
+            mensj.Fecha = mensaje.Fecha.ToString();
             mensj.AutorId = mensaje.UsuarioAutor.Id;
             mensj.ReceptorId = mensaje.UsuarioReceptor.Id;
             mensj.NombreAutor = usuarioENAutor.Nombre;
             mensj.NombreReceptor = usuarioENReceptor.Nombre;
             mensj.NickAutor = usuarioENAutor.Nick;
             mensj.NickReceptor = usuarioENReceptor.Nick;
-            mensj.Titulo = mensaje.Titulo;
-            mensj.Cuerpo = mensaje.Cuerpo;
-            mensj.EstadoLectura = mensaje.EstadoLecutra.ToString();
             mensj.BandejaAutor = mensaje.BandejaAutor.ToString();
             mensj.BandejaReceptor = mensaje.BandejaReceptor.ToString();
 
--- MVC_MultitecUA/Models/MensajeModel.cs
+++ MVC_MultitecUA/Models/MensajeModel.cs
@@ -26,13 +26,15 @@ namespace MVC_MultitecUA.Models
         //[StringLength(maximumLength: 4000, ErrorMessage = "El cuerpo del mensaje no puede tener más de 4000 caracteres")]
         public string Cuerpo { get; set; }
 
+        public string Fecha { get; set; }
+
         public string NombreAutor { get; set; }
-        [Display(Prompt = "Nick del solicitante", Description = "Nick del solicitante", Name = "Nick ")]
+        [Display(Prompt = "Nick del autor", Description = "Nick del autor", Name = "Nick autor")]
         [Required(ErrorMessage = "Debe indicar un nick valido")]
         [StringLength(maximumLength: 90, ErrorMessage = "El nick no puede tener más de 90 caracteres")]
         public string NickAutor { get; set; }
         public string NombreReceptor { get; set; }
-        [Display(Prompt = "Nick del solicitante", Description = "Nick del solicitante", Name = "Nick ")]
+        [Display(Prompt = "Nick del receptor", Description = "Nick del receptor", Name = "Nick receptor")]
         [Required(ErrorMessage = "Debe indicar un nick valido")]
         [StringLength(maximumLength: 90, ErrorMessage = "El nick no puede tener más de 90 caracteres")]
         public string NickReceptor { get; set; }
