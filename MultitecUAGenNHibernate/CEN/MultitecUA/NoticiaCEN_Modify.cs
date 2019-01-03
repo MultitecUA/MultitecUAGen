ce528aec6de9e1215e9d91e2e1232c717ee01ee2
JudithMMolina <33120506+JudithMMolina@users.noreply.github.com>
2019-01-03 12:16:40 +0100

    Segundo commit

---
+++ MultitecUAGenNHibernate/CEN/MultitecUA/NoticiaCEN_Modify.cs
@@ -0,0 +1,41 @@
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Exceptions;
+using MultitecUAGenNHibernate.Exceptions;
+using MultitecUAGenNHibernate.EN.MultitecUA;
+using MultitecUAGenNHibernate.CAD.MultitecUA;
+
+
+/*PROTECTED REGION ID(usingMultitecUAGenNHibernate.CEN.MultitecUA_Noticia_modify) ENABLED START*/
+//  references to other libraries
+/*PROTECTED REGION END*/
+
+namespace MultitecUAGenNHibernate.CEN.MultitecUA
+{
+public partial class NoticiaCEN
+{
+public void Modify (int p_Noticia_OID, string p_titulo, string p_cuerpo, string p_foto)
+{
+        /*PROTECTED REGION ID(MultitecUAGenNHibernate.CEN.MultitecUA_Noticia_modify_customized) START*/
+
+        NoticiaEN noticiaEN = null;
+
+        //Initialized NoticiaEN
+        noticiaEN = new NoticiaEN ();
+        noticiaEN.Id = p_Noticia_OID;
+        noticiaEN.Titulo = p_titulo;
+        noticiaEN.Cuerpo = p_cuerpo;
+        noticiaEN.FotoNoticia = p_foto;
+        //Call to NoticiaCAD
+
+        _INoticiaCAD.Modify (noticiaEN);
+
+        /*PROTECTED REGION END*/
+}
+}
+}
