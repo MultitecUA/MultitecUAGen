970a6da22c8d73980ebe25f4439b67664468da25
JudithMMolina <33120506+JudithMMolina@users.noreply.github.com>
2019-01-07 04:06:34 +0100

    COMMITEANDO TODO

--- MVC_MultitecUA/Controllers/UsuarioController.cs
+++ MVC_MultitecUA/Controllers/UsuarioController.cs
@@ -6,7 +6,9 @@ using MultitecUAGenNHibernate.Enumerated.MultitecUA;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -133,7 +135,7 @@ namespace MVC_MultitecUA.Controllers
 
         // POST: Usuario/Create
         [HttpPost]
-        public ActionResult Create(UsuarioEN usuarioEN)
+        public ActionResult Create(UsuarioEN usuarioEN, HttpPostedFileBase file)
         {
             if (Session["usuario"] == null)
                 return RedirectToAction("Login", "Sesion");
@@ -145,11 +147,83 @@ namespace MVC_MultitecUA.Controllers
             try
             {
                 UsuarioCEN usuarioCEN = new UsuarioCEN();
-                usuarioCEN.New_(usuarioEN.Nombre,usuarioEN.Password,usuarioEN.Foto,usuarioEN.Email,usuarioEN.Nick);
-                return RedirectToAction("Index");
+
+
+                string fileName = "", path = "";
+                if (file != null && file.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(file.FileName);
+                    path = Path.Combine(Server.MapPath("~/Imagenes"), fileName);
+                    if (!System.IO.File.Exists(path))
+                        file.SaveAs(path);
+                    file.SaveAs(path);
+                    fileName = "/Imagenes/" + fileName;
+                }
+
+
+
+                if (usuarioEN.Nombre == null || usuarioEN.Nick == null || usuarioEN.Password == null || usuarioEN.Email == null)
+                {
+                    ViewData["CamposVacios"] = "No pueden haber campos vacíos, excepto por la foto.";
+                    View();
+                }
+
+
+                //VALIDANDO NOMBRE
+                Regex pattern = new Regex("^[A-Za-z áéíóúñç]{1,50}$");
+                if (!pattern.IsMatch(usuarioEN.Nombre))
+                {
+                    ViewData["FormatoNombreUsuario"] = "mal";
+                    return View();
+                }
+
+                //VALIDANDO CONTRASENA
+                pattern = new Regex("^[A-Za-z.-_0-9]{4,15}$");
+                if (!pattern.IsMatch(usuarioEN.Password))
+                {
+                    ViewData["FormatoContrasenaUsuario"] = "De 4 a 15 caracteres. Letras del alfabeto inglés, números y símbolos . - _";
+                    return View();
+                }
+
+
+                //VALIDANDO EMAIL
+                pattern = new Regex("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$");
+                if (!pattern.IsMatch(usuarioEN.Email))
+                {
+                    ViewData["FormatoEmailUsuario"] = "El formato del email no es correcto. Ej: ejemplo@gmail.com";
+                    return View();
+                }
+
+                //VALIDANDO NICK
+                pattern = new Regex("^[A-Za-z.-_]{4,15}$");
+                if (!pattern.IsMatch(usuarioEN.Nick))
+                {
+                    ViewData["FormatoNickUsuario"] = "De 4 a 15 caracteres. Solo letras del alfabeto inglés. Se aceptan los simbolos .-_";
+                    return View();
+                }
+                if (usuarioCEN.ReadNick(usuarioEN.Nick) != null)
+                {
+                    ViewData["NickExiste"] = "Este nick ya existe";
+                    return View();
+                }
+
+
+
+                int idU;
+                if (fileName != "")
+                {
+                    idU = usuarioCEN.New_(usuarioEN.Nombre, usuarioEN.Password, fileName, usuarioEN.Email, usuarioEN.Nick);
+                }
+                else
+                {
+                    idU = usuarioCEN.New_(usuarioEN.Nombre, usuarioEN.Password, "/Imagenes/NoFoto.png", usuarioEN.Email, usuarioEN.Nick);
+                }
+                TempData["usuariocreado"] = "si";
+                return RedirectToAction("Details/" + idU);
             }
             catch
             {
+                ViewData["error"] = "Ha ocurrido un error y no se ha podido crear el usuario";
                 return View();
             }
         }
@@ -172,7 +246,7 @@ namespace MVC_MultitecUA.Controllers
 
         // POST: Usuario/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, UsuarioEN usuarioEN)
+        public ActionResult Edit(int id, UsuarioEN usuarioEN, HttpPostedFileBase file)
         {
             if (Session["usuario"] == null)
                 return RedirectToAction("Login", "Sesion");
@@ -184,13 +258,86 @@ namespace MVC_MultitecUA.Controllers
             try
             {
                 UsuarioCEN usuarioCEN = new UsuarioCEN();
+
+                string fileName = "", path = "";
+                if (file != null && file.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(file.FileName);
+                    path = Path.Combine(Server.MapPath("~/Imagenes"), fileName);
+                    if (!System.IO.File.Exists(path))
+                        file.SaveAs(path);
+                    file.SaveAs(path);
+                    fileName = "/Imagenes/" + fileName;
+                }
+
+                if (usuarioEN.Nombre == null || usuarioEN.Nick == null || usuarioEN.Email == null)
+                {
+                    ViewData["CamposVacios"] = "El nombre, nick o email no pueden ser vacíos.";
+                    View();
+                }
+
+
+                //VALIDANDO NOMBRE
+                Regex pattern = new Regex("^[A-Za-z áéíóúñç]{1,50}$");
+                if (!pattern.IsMatch(usuarioEN.Nombre))
+                {
+                    ViewData["FormatoNombreUsuario"] = "mal";
+                    return View();
+                }
+
+                //VALIDANDO CONTRASENA
+                pattern = new Regex("^[A-Za-z.-_0-9]{4,15}$");
+                if (usuarioEN.Password != null && !pattern.IsMatch(usuarioEN.Password))
+                {
+                    ViewData["FormatoContrasenaUsuario"] = "De 4 a 15 caracteres. Letras del alfabeto inglés, números y símbolos . - _";
+                    return View();
+                }
+
+
+                //VALIDANDO EMAIL
+                pattern = new Regex("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$");
+                if (!pattern.IsMatch(usuarioEN.Email))
+                {
+                    ViewData["FormatoEmailUsuario"] = "El formato del email no es correcto. Ej: ejemplo@gmail.com";
+                    return View();
+                }
+
+                //VALIDANDO NICK
+                pattern = new Regex("^[A-Za-z.-_]{4,15}$");
+                UsuarioEN validando = usuarioCEN.ReadNick(usuarioEN.Nick);
+                if (validando == null)
+                {
+                    if (!pattern.IsMatch(usuarioEN.Nick))
+                    {
+                        ViewData["FormatoNickUsuario"] = "De 4 a 15 caracteres. Solo letras del alfabeto inglés. Se aceptan los simbolos .-_";
+                        return View();
+                    }
+                    if (usuarioCEN.ReadNick(usuarioEN.Nick) != null)
+                    {
+                        ViewData["NickExiste"] = "Este nick ya existe";
+                        return View();
+                    }
+                    Session["usuario"] = usuarioEN.Nick;
+                }
+
                 if (usuarioEN.Password == null)
                     usuarioEN.Password = usuarioCEN.ReadOID(id).Password;
-                usuarioCEN.Modify(id, usuarioEN.Nombre, usuarioEN.Password,usuarioEN.Email,usuarioEN.Nick,usuarioEN.Foto);
-                return RedirectToAction("Index");
+
+                TempData["usuarioeditado"] = "si";
+                if (fileName != "")
+                {
+                    usuarioCEN.Modify(id, usuarioEN.Nombre, usuarioEN.Password, usuarioEN.Email, usuarioEN.Nick, fileName);
+                }
+                else
+                {
+                    usuarioCEN.Modify(id, usuarioEN.Nombre, usuarioEN.Password, usuarioEN.Email, usuarioEN.Nick, "/Imagenes/NoFoto.png");
+                }
+
+                return RedirectToAction("Details/" + id);
             }
             catch
             {
+                ViewData["error"] = "Ha ocurrido un error y no se ha podido editar el usuario.";
                 return View();
             }
         }
@@ -209,7 +356,7 @@ namespace MVC_MultitecUA.Controllers
 
         // POST: Usuario/Modificar/5
         [HttpPost]
-        public ActionResult Modificar(int id, FormCollection formCollection)
+        public ActionResult Modificar(int id, FormCollection formCollection, HttpPostedFileBase file)
         {
 
             if (Session["usuario"] == null)
@@ -224,11 +371,83 @@ namespace MVC_MultitecUA.Controllers
                 if (usuarioCEN.Login(id, formCollection["Password"]) != null)
                 {
 
+                    string fileName = "", path = "";
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        fileName = Path.GetFileName(file.FileName);
+                        path = Path.Combine(Server.MapPath("~/Imagenes"), fileName);
+                        if (!System.IO.File.Exists(path))
+                            file.SaveAs(path);
+                        file.SaveAs(path);
+                        fileName = "/Imagenes/" + fileName;
+                    }
+
+                    if (formCollection["Nombre"] == "" || formCollection["Nick"] == "" || formCollection["Email"] == "")
+                    {
+                        ViewData["CamposVacios"] = "El nombre, nick o email no pueden ser vacíos.";
+                        return View("./VistaUsuario/Modificar", usuarioEN);
+                    }
+
+
+                    //VALIDANDO NOMBRE
+                    Regex pattern = new Regex("^[A-Za-z áéíóúñç]{1,50}$");
+                    if (!pattern.IsMatch(formCollection["Nombre"]))
+                    {
+                        ViewData["FormatoNombreUsuario"] = "mal";
+                        return View("./VistaUsuario/Modificar", usuarioEN);
+                    }
+
+                    //VALIDANDO CONTRASENA
+                    pattern = new Regex("^[A-Za-z.-_0-9]{4,15}$");
                     if (formCollection["newpass"] == "")
                     {
                         formCollection["newpass"] = formCollection["Password"];
                     }
-                    usuarioCEN.Modify(id, formCollection["Nombre"], formCollection["newpass"], formCollection["Email"], formCollection["Nick"], formCollection["Foto"]);
+                    else if (!pattern.IsMatch(formCollection["newpass"]))
+                    {
+                        ViewData["FormatoContrasenaUsuario"] = "De 4 a 15 caracteres. Letras del alfabeto inglés, números y símbolos . - _";
+                        return View("./VistaUsuario/Modificar", usuarioEN);
+                    }
+
+
+                    //VALIDANDO EMAIL
+                    pattern = new Regex("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$");
+                    if (!pattern.IsMatch(formCollection["Email"]))
+                    {
+                        ViewData["FormatoEmailUsuario"] = "El formato del email no es correcto. Ej: ejemplo@gmail.com";
+                        return View("./VistaUsuario/Modificar", usuarioEN);
+                    }
+
+                    //VALIDANDO NICK
+                    pattern = new Regex("^[A-Za-z.-_]{4,15}$");
+                    UsuarioEN validando = usuarioCEN.ReadNick(usuarioEN.Nick);
+                    if (validando == null)
+                    {
+                        if (!pattern.IsMatch(formCollection["Nick"]))
+                        {
+                            ViewData["FormatoNickUsuario"] = "De 4 a 15 caracteres. Solo letras del alfabeto inglés. Se aceptan los simbolos .-_";
+                            return View("./VistaUsuario/Modificar", usuarioEN);
+                        }
+                        if (usuarioCEN.ReadNick(formCollection["Nick"]) != null)
+                        {
+                            ViewData["NickExiste"] = "Este nick ya existe";
+
+                            return View("./VistaUsuario/Modificar", usuarioEN);
+                        }
+                        Session["usuario"] = usuarioEN.Nick;
+                    }
+
+
+                    if (fileName != "")
+                    {
+                        usuarioCEN.Modify(id, formCollection["Nombre"], formCollection["newpass"], formCollection["Email"], formCollection["Nick"], fileName);
+                    }
+                    else
+                    {
+                        usuarioCEN.Modify(id, formCollection["Nombre"], formCollection["newpass"], formCollection["Email"], formCollection["Nick"], "/Imagenes/NoFoto.png");
+
+                    }
+
                     if (formCollection["Nick"].ToString() != Session["usuario"].ToString())
                         Session["usuario"] = formCollection["Nick"];
 
@@ -236,7 +455,7 @@ namespace MVC_MultitecUA.Controllers
                     CategoriaUsuarioCEN categorias = new CategoriaUsuarioCEN();
                     List<CategoriaUsuarioEN> cat = categorias.ReadAll(0, -1).ToList();
 
-                    
+
                     foreach (CategoriaUsuarioEN a in cat)
                     {
                         if (usuarioEN.CategoriasUsuarios.Contains(a))
@@ -245,6 +464,7 @@ namespace MVC_MultitecUA.Controllers
                         }
                     }
                     ViewData["listaCategoriasEliminar"] = listaCatesE;
+                    TempData["Modificar"] = "Tu perfil ha modificado";
                     return View("./VistaUsuario/Detalles", usuarioEN);
                 }
                 ViewData["error"] = "La contraseña introducida no es correcta";
@@ -252,6 +472,7 @@ namespace MVC_MultitecUA.Controllers
             }
             catch
             {
+                ViewData["error2"] = "Ha ocurrido un error y no se ha podido editar el usuario.";
                 return View();
             }
         }
